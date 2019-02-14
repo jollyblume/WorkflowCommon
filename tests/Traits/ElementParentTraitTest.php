4e008c5360a5a56d79a53596d99179242b06d5e0
Jolly Blume <jollyblume2@gmail.com>
2019-02-14 07:46:50 -0700

    more phpmd

--- tests/Traits/ElementParentTraitTest.php
+++ tests/Traits/ElementParentTraitTest.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use JBJ\Workflow\Traits\ElementParentTrait;
 use PHPUnit\Framework\TestCase;
 
+/** @SuppressWarnings(PHPMD.TooManyPublicMethods) */
 class ElementParentTraitTest extends TestCase
 {
     public function testIfEmpty()
