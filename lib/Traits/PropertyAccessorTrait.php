a73b42057bc2743bc2d4a239ac2ed4f6b2b01ef4
Jolly Blume <jollyblume2@gmail.com>
2019-02-14 11:49:49 -0700

    added setpropertyaccessorhere

--- lib/Traits/PropertyAccessorTrait.php
+++ lib/Traits/PropertyAccessorTrait.php
@@ -8,6 +8,7 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 trait PropertyAccessorTrait
 {
     private $propertyAccessor;
+    private $persistPropertyAccessorHere = false;
 
     public function getPropertyAccessor()
     {
@@ -19,13 +20,29 @@ trait PropertyAccessorTrait
         $this->propertyAccessor = $propertyAccessor;
     }
 
+    protected function persistPropertyAccessorHere()
+    {
+        return boolval($this->persistPropertyAccessorHere);
+    }
+
+    protected function setPersistPropertyAccessorHere(bool $persist)
+    {
+        $this->persistPropertyAccessorHere = $persist;
+    }
+
     public function findPropertyAccessor()
     {
         $propertyAccessor = $this->propertyAccessor;
         if (null === $propertyAccessor && method_exists($this, 'getValueForMethod')) {
             $propertyAccessor = $this->getValueForMethod('getPropertyAccessor');
         }
-        return $propertyAccessor ?: $this->createPropertyAccessor();
+        if (!$propertyAccessor instanceof PropertyAccessorInterface) {
+            $propertyAccessor = $this->createPropertyAccessor();
+        }
+        if ($this->persistPropertyAccessorHere()) {
+            $this->setPropertyAccessor($propertyAccessor);
+        }
+        return $propertyAccessor;
     }
 
     /** @SuppressWarnings(PHPMD.StaticAccess) */
