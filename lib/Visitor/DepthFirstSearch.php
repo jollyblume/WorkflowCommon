7da41b8c563f3301e65bf5c2aede68e53a05892a
Jolly Blume <jollyblume2@gmail.com>
2019-02-24 10:23:30 -0700

    initial

---
+++ lib/Visitor/DepthFirstSearch.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace JBJ\Workflow\Visitor;
+
+use JBJ\Workflow\Visitor\NodeVisitorInterface;
+use JBJ\Workflow\NodeCollectionInterface;
+
+class DepthFirstSearch implements NodeVisitorInterface
+{
+    private $visited = [];
+    private $visitor;
+    private $currentPath;
+
+    public function visit(NodeCollectionInterface $node)
+    {
+        $currentPath = $this->currentPath;
+        $visited = boolval($this->visited[$currentPath]);
+        if (!$visited) {
+            $this->visitor->visit($node);
+        }
+        $this->visited[$currentPath]++;
+    }
+
+    public function traverse(NodeCollectionInterface $node, NodeVisitorInterface $visitor, string $currentPath = '/')
+    {
+        $this->visitor = $visitor;
+        $this->visited[$currentPath] = 0;
+        $this->$currentPath = $currentPath;
+        $node->accept($this);
+        if (!$node->isLeafNode()) {
+            $currentPath = rtrim($currentPath, '/');
+            foreach ($node as $child) {
+                $childPath = sprintf('/%s/%s', $currentPath, $child->getName());
+                $paths = $this->traverse($child, $visitor, $childPath);
+                $this->visited = array_merge($this->visited, $paths);
+            }
+        }
+        return $this->visited;
+    }
+}
