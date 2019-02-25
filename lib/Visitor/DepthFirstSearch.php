2999781f426748d6c4485e3e447b660ef965a606
Jolly Blume <jollyblume2@gmail.com>
2019-02-25 11:11:29 -0700

    new innards

--- lib/Visitor/DepthFirstSearch.php
+++ lib/Visitor/DepthFirstSearch.php
@@ -5,38 +5,21 @@ namespace JBJ\Workflow\Visitor;
 use JBJ\Workflow\Visitor\NodeVisitorInterface;
 use JBJ\Workflow\NodeCollectionInterface;
 
-class DepthFirstSearch implements NodeVisitorInterface
+class DepthFirstSearch
 {
-    private $visited = [];
-    private $visitor;
-    private $currentPath;
-
-    public function visit(NodeCollectionInterface $node)
-    {
-        $currentPath = $this->currentPath;
-        $visited = boolval($this->visited[$currentPath]);
-        if (!$visited) {
-            $visitor = $this->visitor;
-            $visitor->visit($node);
-        }
-        $this->visited[$currentPath]++;
-    }
-
     protected function innerTraverse(NodeCollectionInterface $node, NodeVisitorInterface $visitor, string $currentPath)
     {
-        $this->visitor = $visitor;
-        $this->visited[$currentPath] = 0;
-        $this->currentPath = $currentPath;
-        $node->accept($this);
+        $paths = [$currentPath => 1];
+        $node->accept($visitor);
         if (!$node->isLeafNode()) {
             $currentPath = rtrim($currentPath, '/');
             foreach ($node as $child) {
                 $childPath = sprintf('%s/%s', $currentPath, $child->getName());
-                $paths = $this->traverse($child, $visitor, $childPath);
-                $this->visited = array_merge($this->visited, $paths);
+                $innerPaths = $this->traverse($child, $visitor, $childPath);
+                $paths = array_merge($paths, $innerPaths);
             }
         }
-        return $this->visited;
+        return $paths;
     }
 
     public function traverse(NodeCollectionInterface $node, NodeVisitorInterface $visitor)
